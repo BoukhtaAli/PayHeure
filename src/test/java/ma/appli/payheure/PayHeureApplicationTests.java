da32cda4a85d6f607cf186d5d78cd8ce8b87d6d7
Ali Boukhta <ali.boukhta.1997@gmail.com>
2026-08-19 14:40:37 +0100

    init projet

---
+++ src/test/java/ma/appli/payheure/PayHeureApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.appli.payheure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayHeureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
